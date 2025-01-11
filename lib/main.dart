4d5c5de44726988d7cc8d7d5fa6f23eb0c0b51dd
MJJP1364 <m.j.j.p1364@gmail.com>
2025-01-11 21:08:28 +0330

    add getx package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_getx_full_course/navigation/navigation.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const MyApp());
 }
 
