9aaa6ad69af0876ba506d2b51736a694eacb1f56
MJJP1364 <m.j.j.p1364@gmail.com>
2025-01-11 21:01:36 +0330

    create dev branch

---
+++ lib/navigation/navigation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetNavigation extends StatelessWidget {
+  const GetNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
